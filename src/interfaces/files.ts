6eee4e145cd84f72724b09655dc39c6d3e330d4b
kibuniverse <3050540005@qq.com>
2020-08-19 19:35:55 +0800

    feat: 完善更新组件状态接口

--- src/interfaces/files.ts
+++ src/interfaces/files.ts
@@ -1,9 +1,16 @@
 export interface files {
-    readonly filesList: FileList | []
     readonly waitUploadFiles: Array<IwaitUploadFiles> | []
+    readonly addFiles: FileList | []
+    readonly waitCalculateFiles: Array<IwaitCalculateFiles> | []
 }
 
 export interface IwaitUploadFiles {
     readonly file: File
     readonly hash: string
+}
+
+export interface IwaitCalculateFiles {
+    id: string
+    file: File
+    isCalculate: boolean
 }
