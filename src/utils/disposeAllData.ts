5c49d1d3a6b60a7c903def1ddf299272c06f6a23
kibuniverse <3050540005@qq.com>
2020-08-25 11:26:18 +0800

    feat: 增加上传切片数量

--- src/utils/disposeAllData.ts
+++ src/utils/disposeAllData.ts
@@ -5,24 +5,26 @@ import { calculatehash } from './createHash'
 
 export interface Iprops {
     chunkSize?: number
-    addNewWaitCalculateFile: (files: Array<IwaitCalculateFile>) => void
-    addNewWaitUploadFile: (file: IwaitUploadFile) => void
+    updateWaitCalculateFile: (files: Array<IwaitCalculateFile>) => void
+    updateWaitUploadFile: (files: Array<IwaitUploadFile>) => void
 }
 
 export default class DisposeAllData implements UtilClassInterface {
     waitCalculateFiles = [] as Array<IwaitCalculateFile>
     waitUploadFiles = [] as Array<IwaitUploadFile>
     uploadingFiles = [] as Array<IuploadingFile>
-    addNewWaitCalculateFile: (files: Array<IwaitCalculateFile>) => void
-    addNewWaitUploadFile: (file: IwaitUploadFile) => void
+    updateWaitCalculateFile: (files: Array<IwaitCalculateFile>) => void
+    updateWaitUploadFile: (files: Array<IwaitUploadFile>) => void
     isCalculating: boolean
     chunkSize: number
+    chunksConcurUploadNum: number
     constructor(props: Iprops) {
         this.isCalculating = false
         // 切片大小默认4M
         this.chunkSize = props.chunkSize ? props.chunkSize : 4 * 1024 * 1024
-        this.addNewWaitCalculateFile = props.addNewWaitCalculateFile
-        this.addNewWaitUploadFile = props.addNewWaitUploadFile
+        this.updateWaitCalculateFile = props.updateWaitCalculateFile
+        this.updateWaitUploadFile = props.updateWaitUploadFile
+        this.chunksConcurUploadNum = parseInt(String(10 / this.waitUploadFiles.length))
     }
     /**
      * 
@@ -35,7 +37,7 @@ export default class DisposeAllData implements UtilClassInterface {
                 file: newFiles[i],
             })
         }
-        this.addNewWaitCalculateFile(this.waitCalculateFiles)
+        this.updateWaitCalculateFile(this.waitCalculateFiles)
         if (!this.isCalculating) {
             this.calculateFilesMessage()
         }
@@ -48,21 +50,43 @@ export default class DisposeAllData implements UtilClassInterface {
         while (this.waitCalculateFiles.length > 0) {
             let file: any = this.waitCalculateFiles.shift()?.file
             let waituploadFile: IwaitUploadFile = {
+                id: `${<File>file.name}_${new Date().getTime()}`,
                 file: file,
-                chunkList: getFileChunkList(file, this.chunkSize)
+                chunkList: getFileChunkList(file, this.chunkSize),
+                uploadProcess: 50
             }
             let hash: any = await calculatehash(waituploadFile.chunkList)
             waituploadFile.chunkList.forEach((item: chunkListsFile, index: number) => {
                 item.hash = `${hash}_${index}`
             })
-            console.log(`完成文件${<File>file.name}hash计算`)
+            waituploadFile.hash = hash
+            console.log(`完成文件${file.name}hash计算`)
             this.addCalculatedFile(waituploadFile)
+            this.updateWaitCalculateFile(this.waitCalculateFiles)
+            this.updateChunksConcurUploadNum()
+            this.uploadFile(this.waitUploadFiles.length - 1)
         }
         this.isCalculating = false
+        this.updateChunksConcurUploadNum()
+        this.UploadMultipleFiles()
+    }
+
+    private updateChunksConcurUploadNum () {
+        this.chunksConcurUploadNum = parseInt(String(10 / this.waitUploadFiles.length))
     }
 
     private addCalculatedFile(newWaitUploadFile: IwaitUploadFile) {
         this.waitUploadFiles.push(newWaitUploadFile)
+        this.updateWaitUploadFile(this.waitUploadFiles)
+    }
+
+
+    public uploadFile(index: number) {
+
+    }
+
+    public UploadMultipleFiles() {
+        console.log(this.chunksConcurUploadNum)
     }
 
 }
