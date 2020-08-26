f61cbc8e9be3d239ae2c59d21dd75d9d7584c57b
kibuniverse <3050540005@qq.com>
2020-08-26 19:53:13 +0800

    style: 修改style

--- src/components/UploadProcessBar.tsx
+++ src/components/UploadProcessBar.tsx
@@ -5,6 +5,10 @@ interface Iprops {
 
 
 const UploadProcess = (props: Iprops) => {
+    let process = props.uploadProcess
+    while(process > 1) {
+        process = process / 10
+    }
     return (
         <div style={{
             width: '100%',
@@ -13,7 +17,7 @@ const UploadProcess = (props: Iprops) => {
             borderRadius: '2px',
         }}>
             <div style={{
-                width: `${props.uploadProcess * 100}%`,
+                width: `${process * 100}%`,
                 height: '4px',
                 backgroundImage: 'linear-gradient(to right, #8ebeb9, #3bc9d7, #37b9e9)',
                 transition: 'all .5s'
