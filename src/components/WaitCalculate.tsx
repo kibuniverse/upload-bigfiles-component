19a54299f42d5d5fd21cac05a00d342ad73e4753
kibuniverse <3050540005@qq.com>
2020-08-26 19:54:00 +0800

    style: 修改文件间距

--- src/components/WaitCalculate.tsx
+++ src/components/WaitCalculate.tsx
@@ -7,19 +7,30 @@ export interface Iprops {
 }
 
 const WaitCalculateFiles = (props: Iprops) => {
+    const listItem = props.files.map((item: IwaitCalculateFile) => (
+        <li key={item.id}>
+            {item.file.name}
+        </li>
+    ))
     return (
-        <div>
-            <div style={{
-                width: '400px',
-                margin: 'auto',
-                borderRadius: '10px',
-                textAlign: 'center',
-                color: 'blue',
-                padding: '10px',
-                border: '1px solid #5099ed',
-                backgroundColor: '#b8d7fb',
-            }}>正在计算以下文件哈希，请稍等</div>
-        </div >
+        <div style={{
+            width: '400px',
+            margin: 'auto',
+            borderRadius: '10px',
+            textAlign: 'center',
+            color: 'blue',
+            padding: '10px',
+            border: '1px solid #5099ed',
+            backgroundColor: '#b8d7fb',
+        }}>
+            <p>正在计算以下文件哈希，请稍等</p>
+            <ul style={{
+                listStyle: 'none',
+                padding: '0px'
+            }}>
+                {listItem}
+            </ul>
+        </div>
     )
 }
 export default WaitCalculateFiles
