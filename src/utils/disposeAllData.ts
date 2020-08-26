a899f2e2c677cee672392efac7f2e01073d1d26d
kibuniverse <3050540005@qq.com>
2020-08-26 20:02:22 +0800

    feat: 😋

--- src/utils/disposeAllData.ts
+++ src/utils/disposeAllData.ts
@@ -2,6 +2,11 @@ import { UtilClassInterface } from '../interfaces/utilsClassInterface'
 import { IwaitCalculateFile, IwaitUploadFile, IuploadingFile, chunkListsFile } from '../interfaces/interfaces'
 import getFileChunkList from './getFileChunkHash'
 import { calculatehash } from './createHash'
+import uploadFile from './uploadFile'
+import request from './request'
+import { send } from 'process'
+import { wait } from '@testing-library/react'
+
 
 export interface Iprops {
     chunkSize?: number
@@ -17,14 +22,14 @@ export default class DisposeAllData implements UtilClassInterface {
     updateWaitUploadFile: (files: Array<IwaitUploadFile>) => void
     isCalculating: boolean
     chunkSize: number
-    chunksConcurUploadNum: number
+    chunksConcurrenceUploadNum: number
     constructor(props: Iprops) {
         this.isCalculating = false
         // 切片大小默认4M
         this.chunkSize = props.chunkSize ? props.chunkSize : 4 * 1024 * 1024
         this.updateWaitCalculateFile = props.updateWaitCalculateFile
         this.updateWaitUploadFile = props.updateWaitUploadFile
-        this.chunksConcurUploadNum = parseInt(String(10 / this.waitUploadFiles.length))
+        this.chunksConcurrenceUploadNum = parseInt(String(10 / this.waitUploadFiles.length))
     }
     /**
      * 
@@ -38,19 +43,16 @@ export default class DisposeAllData implements UtilClassInterface {
             })
         }
         this.updateWaitCalculateFile(this.waitCalculateFiles)
-        if (!this.isCalculating) {
-            this.calculateFilesMessage()
-        }
+        this.calculateFilesMessage()
     }
     /**
      * calculateFilesMessage 获取文件切片以及hash
      */
     private async calculateFilesMessage() {
-        this.isCalculating = true
         while (this.waitCalculateFiles.length > 0) {
             let file: any = this.waitCalculateFiles.shift()?.file
             let waituploadFile: IwaitUploadFile = {
-                id: `${<File>file.name}_${new Date().getTime()}`,
+                id: `${file.name as File}_${new Date().getTime()}`,
                 file: file,
                 chunkList: getFileChunkList(file, this.chunkSize),
                 uploadProcess: 50
@@ -58,35 +60,40 @@ export default class DisposeAllData implements UtilClassInterface {
             let hash: any = await calculatehash(waituploadFile.chunkList)
             waituploadFile.chunkList.forEach((item: chunkListsFile, index: number) => {
                 item.hash = `${hash}_${index}`
+                item.index = index
             })
             waituploadFile.hash = hash
             console.log(`完成文件${file.name}hash计算`)
+            // 更新计算完成文件数组
             this.addCalculatedFile(waituploadFile)
+            // 上报新的待计算文件数组
             this.updateWaitCalculateFile(this.waitCalculateFiles)
-            this.updateChunksConcurUploadNum()
+            this.updateChunksConcurrenceUploadNum()
             this.uploadFile(this.waitUploadFiles.length - 1)
         }
-        this.isCalculating = false
-        this.updateChunksConcurUploadNum()
-        this.UploadMultipleFiles()
+        this.updateChunksConcurrenceUploadNum()
     }
 
-    private updateChunksConcurUploadNum () {
-        this.chunksConcurUploadNum = parseInt(String(10 / this.waitUploadFiles.length))
+    private updateChunksConcurrenceUploadNum() {
+        this.chunksConcurrenceUploadNum = parseInt(String(10 / this.waitUploadFiles.length))
     }
 
-    private addCalculatedFile(newWaitUploadFile: IwaitUploadFile) {
+    private async addCalculatedFile(newWaitUploadFile: IwaitUploadFile) {
         this.waitUploadFiles.push(newWaitUploadFile)
         this.updateWaitUploadFile(this.waitUploadFiles)
+        let p = await uploadFile(newWaitUploadFile, 3, (e: any): void => { })
+        await this.mergeRequest()
     }
 
+    private mergeRequest() {
 
+    }
     public uploadFile(index: number) {
 
     }
 
     public UploadMultipleFiles() {
-        console.log(this.chunksConcurUploadNum)
+        console.log(this.chunksConcurrenceUploadNum)
     }
 
 }
