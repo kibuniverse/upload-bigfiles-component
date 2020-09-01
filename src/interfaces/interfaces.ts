9191c7b2b82e20c017fb11a174a8bed3f6b78eb7
kibuniverse <3050540005@qq.com>
2020-09-01 20:46:46 +0800

    refactor: 完善接口继承

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,8 +1,13 @@
 export interface IfilesStatus {
     waitUploadFiles: Array<IwaitUploadFile> | []
     waitCalculateFiles: Array<IwaitCalculateFile> | []
-    uploadingFiles: Array<IuploadingFile> | []
     uploadedFiles: Array<IuploadedFile> | []
+}
+
+
+interface fileBasicMessage {
+    file: File,
+    id?: string
 }
 
 export interface chunkListsFile {
@@ -12,26 +17,17 @@ export interface chunkListsFile {
     index?: number
 }
 
-export interface IcalculatedBasic {
-    chunkList: Array<chunkListsFile> | []
-}
-
-export interface IwaitUploadFile extends IcalculatedBasic {
-    id?: string
-    file: File
+export interface IwaitUploadFile extends fileBasicMessage {
     hash?: string
     uploadProcess?: number
     uploadPercentArr: Array<number> | []
+    chunkList: Array<chunkListsFile> | []
     uploadedSize: number
 }
 
 export interface IwaitCalculateFile {
     id: string
     file: File
-}
-
-export interface IuploadingFile extends IwaitCalculateFile, IcalculatedBasic {
-    uploadProcess: number
 }
 
 export interface IuploadedFile {
