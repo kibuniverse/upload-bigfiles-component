23a214605864d51dac77062db00e9666e098a4c7
yankaizhi <yankaizhi@bytedance.com>
2021-10-27 12:31:03 +0800

    docs: 删除 tsc 后的 js 文件

--- src/utils/Apiurl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ipUrl = 'http://49.234.79.241:8001/';
-var servicePath = {
-    sendChunkRequest: ipUrl + 'file_upload',
-    mergeRequest: ipUrl + 'mergeReq',
-    verify: ipUrl + 'verify'
-};
-exports["default"] = servicePath;
--- src/utils/getExtendName.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function getExtendName(nameStr) {
-    return nameStr.split('.')[nameStr.split('.').length - 1];
-}
-exports["default"] = getExtendName;
